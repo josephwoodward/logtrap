2cfc6d4eb0e4567890a2d7f74061e6823b6e75bf
Joseph Woodward <joseph.woodward@xeuse.com>
2025-03-11 22:55:49 +0000

    tidy up benchmarks
    tidy up benchmark

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,7 +2,6 @@ package logtrap_test
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log/slog"
 	"testing"
@@ -11,19 +10,16 @@ import (
 )
 
 var table = []struct {
-	name  string
-	h     *slog.Logger
-	input int
+	name string
+	h    *slog.Logger
 }{
 	{
-		name:  "default handler",
-		h:     slog.New(defaultHandler()),
-		input: 100,
+		name: "default handler",
+		h:    slog.New(defaultHandler()),
 	},
 	{
-		name:  "ring buffer handler",
-		h:     slog.New(slogHandler()),
-		input: 100,
+		name: "ring buffer handler",
+		h:    slog.New(slogHandler()),
 	},
 }
 
@@ -40,48 +36,18 @@ func defaultHandler() slog.Handler {
 	return handler
 }
 
-// func Test_Benchmark(t *testing.T) {
-// 	logger := slog.New(slogHandler())
-// 	ctx := context.WithValue(context.Background(), "request_id", "1234")
-// 	msg := "hello world!"
-
-// 	logger.InfoContext(ctx, msg)
-// 	logger.WarnContext(ctx, msg)
-// 	logger.ErrorContext(ctx, msg)
-// }
-
-func BenchmarkPrimeNumbers(b *testing.B) {
-	ctx := context.WithValue(context.Background(), "request_id", "1234")
+func Benchmark_RequestScopedLogs(b *testing.B) {
+	ctx := context.Background()
 	msg := "hello world!"
+	attr := "request_id"
+
 	for _, v := range table {
 		b.Run(v.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				v.h.InfoContext(ctx, msg, "request_id", fmt.Sprintf("request_%d", i))
-				// v.h.WarnContext(ctx, msg)
-				// v.h.WarnContext(ctx, msg)
-				// v.h.InfoContext(ctx, msg)
-				// v.h.WarnContext(ctx, msg)
-				// v.h.InfoContext(ctx, msg)
-				// v.h.ErrorContext(ctx, msg)
+				v.h.InfoContext(ctx, msg, attr, i)
 			}
 		})
 	}
+
 	b.ReportAllocs()
-}
-
-// func BenchmarkBubbleSort(b *testing.B) {
-// 	h := logring.NewHandler(
-// 		slog.NewTextHandler(io.Discard, &slog.HandlerOptions{ReplaceAttr: clearTimeAttr, Level: slog.LevelDebug}),
-// 		&logring.HandlerOptions{TailSize: 10, TailLevel: slog.LevelInfo, AttrKey: "request_id", FlushLevel: slog.LevelError},
-// 	)
-// 	logger := slog.New(h)
-// 	ctx := context.WithValue(context.Background(), "request_id", "1234")
-// 	for i := 0; i < b.N; i++ {
-// 		logSomething(ctx, logger)
-// 	}
-// 	b.ReportAllocs()
-// }
-
-func logSomething(ctx context.Context, logger *slog.Logger) {
-	slog.InfoContext(ctx, "Hello world!")
 }
